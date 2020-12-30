c74e565a77b52a5321853aedc988bae4803761cf
Jan Bissinger (CI/CBW2) <jan.bissinger@sg.bosch.com>
2020-12-30 19:16:45 +0800

    Bump version

--- lib/common/config.dart
+++ lib/common/config.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class Config {
   static const String APP_NAME = "Countdown Tracker";
-  static const String VERSION = "0.0.1";
+  static const String VERSION = "1.1.0";
   static const Color PRIMARY_COLOR = Colors.blue;
   static const UnderlineInputBorder BORDER = UnderlineInputBorder(
     borderSide: BorderSide(color: PRIMARY_COLOR),
